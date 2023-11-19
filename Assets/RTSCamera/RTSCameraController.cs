41d3050cf27fca1a54b6acc0c986334fe1b32610
Ryan Koning <rkoning8@gmail.com>
2023-11-19 13:16:51 -0800

    Fix linting on camera controller

--- Assets/RTSCamera/RTSCameraController.cs
+++ Assets/RTSCamera/RTSCameraController.cs
@@ -3,67 +3,76 @@ using System.Collections.Generic;
 using UnityEditor.ShaderGraph;
 using UnityEngine;
 using UnityEngine.Video;
-
-public class RTSCameraController : MonoBehaviour
-{
-    public Camera mainCamera;
-    public Transform cameraRotationPivot;
-
-    public float rotationSpeed;
-    public float zoomSpeed;
-    public float minZoom = 10f, maxZoom = 50f;
-    public float minZoomAngle = 35, maxZoomAngle = 70;
-    public float panSpeed;
-
-    private Vector2 panInput;
-    private float zoomInput;
-    private float rotationInput;
-
-    // Start is called before the first frame update
-    void Start()
+namespace rkoning.RTS {
+    public class RTSCameraController : MonoBehaviour
     {
-        if (!mainCamera) {
-            mainCamera = Camera.main;
+        [SerializeField]
+        private Camera mainCamera;
+        [SerializeField]
+        private Transform cameraRotationPivot;
+
+
+        [SerializeField]
+        private float rotationSpeed;
+        [SerializeField]
+        private float zoomSpeed;
+        [SerializeField]
+        private float minZoom = 10f, maxZoom = 50f;
+        [SerializeField]
+        private float minZoomAngle = 35, maxZoomAngle = 70;
+        [SerializeField]
+        private float panSpeed;
+
+        private Vector2 panInput;
+        private float zoomInput;
+        private float rotationInput;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            if (!mainCamera) {
+                mainCamera = Camera.main;
+            }
         }
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        CaptureInputs();
-        MoveCamera();
-    }
+        // Update is called once per frame
+        void Update()
+        {
+            CaptureInputs();
+            MoveCamera();
+        }
 
-    void MoveCamera() {
-        var flatDirection = new Vector3(transform.forward.x, 0f, transform.forward.z).normalized;
-        Quaternion rotation = Quaternion.LookRotation(flatDirection);
+        void MoveCamera() {
+            var flatDirection = new Vector3(transform.forward.x, 0f, transform.forward.z).normalized;
+            Quaternion rotation = Quaternion.LookRotation(flatDirection);
 
-        var delta = rotation * new Vector3(panInput.x * panSpeed, zoomInput * zoomSpeed, panInput.y * panSpeed) * Time.deltaTime;
+            var delta = rotation * new Vector3(panInput.x * panSpeed, zoomInput * zoomSpeed, panInput.y * panSpeed) * Time.deltaTime;
 
-        Vector3 nextPosition = transform.position + delta;
-        float clampedY = Mathf.Clamp(nextPosition.y, minZoom, maxZoom);
-        nextPosition.y = clampedY;
-        transform.position = nextPosition;
+            Vector3 nextPosition = transform.position + delta;
+            float clampedY = Mathf.Clamp(nextPosition.y, minZoom, maxZoom);
+            nextPosition.y = clampedY;
+            transform.position = nextPosition;
 
-        transform.Rotate(Vector3.up, rotationInput * rotationSpeed * Time.deltaTime);
-        float zoomPercentage = (clampedY - minZoom) / (maxZoom - minZoom);
-       
-        float zoomAngle = minZoomAngle + (maxZoomAngle - minZoomAngle) * zoomPercentage;
-        cameraRotationPivot.localEulerAngles = new Vector3(zoomAngle, 0f, 0f);
-    }
+            transform.Rotate(Vector3.up, rotationInput * rotationSpeed * Time.deltaTime);
+            float zoomPercentage = (clampedY - minZoom) / (maxZoom - minZoom);
+        
+            float zoomAngle = minZoomAngle + (maxZoomAngle - minZoomAngle) * zoomPercentage;
+            cameraRotationPivot.localEulerAngles = new Vector3(zoomAngle, 0f, 0f);
+        }
 
-    void CaptureInputs() {
-        // TODO: Pan if mouse at edge of screen
-        panInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")).normalized;
+        void CaptureInputs() {
+            // TODO: Pan if mouse at edge of screen
+            panInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")).normalized;
 
-        zoomInput = -Input.mouseScrollDelta.y;
+            zoomInput = -Input.mouseScrollDelta.y;
 
-        if (Input.GetKey(KeyCode.Q)) {
-            rotationInput = -1;
-        } else if (Input.GetKey(KeyCode.E)) {
-            rotationInput = 1;
-        } else {
-            rotationInput = 0;
+            if (Input.GetKey(KeyCode.Q)) {
+                rotationInput = -1;
+            } else if (Input.GetKey(KeyCode.E)) {
+                rotationInput = 1;
+            } else {
+                rotationInput = 0;
+            }
         }
     }
 }
